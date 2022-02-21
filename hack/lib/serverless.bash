da5708b344f79ae58f5be62252d6dc6bccbe1403
Martin Gencur <mgencur@redhat.com>
2022-02-21 10:06:43 +0100

    TMP: Waiting before dumping state
    * give time to inspect cluster logging and Kafka before dumping the
    remaining state

--- hack/lib/serverless.bash
+++ hack/lib/serverless.bash
@@ -389,6 +389,8 @@ function dump_state.setup {
 }
 
 function dump_state {
+  logger.info "Waiting 1 hour before dumping state..."
+  sleep 3600
   logger.info 'Dumping state...'
   logger.debug 'Environment variables:'
   env
