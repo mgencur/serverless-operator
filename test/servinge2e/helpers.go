84b1f67a36aac273f508f4db3065d549810123e8
Martin Gencur <mgencur@redhat.com>
2022-10-17 11:13:35 +0200

    Move image definitions to images.go

--- test/servinge2e/helpers.go
+++ test/servinge2e/helpers.go
@@ -12,7 +12,6 @@ import (
 )
 
 const (
-	image                 = "quay.io/openshift-knative/helloworld-go"
 	helloworldService     = "helloworld-go"
 	helloworldService2    = "helloworld-go2"
 	kubeHelloworldService = "kube-helloworld-go"
